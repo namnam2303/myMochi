873043378dcd3704f33b0cf77647c66309af2290
namnam2303 <imsofh@gmail.com>
2024-01-09 23:03:25 +0700

    Initial project

---
+++ MyMochi/src/test/java/com/example/MyMochi/MyMochiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyMochi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMochiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
